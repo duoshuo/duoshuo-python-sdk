eca5f674246896dd300b6010bbfad4eec73be685
perchouli <jp.chenyang@gmail.com>
2012-03-17 00:16:15 +0800

    ADD: templatetags

--- duoshuo/models.py
+++ duoshuo/models.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+AUTH_PROFILE_MODULE = getattr(settings, "AUTH_PROFILE_MODULE")
+
+class Site(models.Model):
+    use_images = models.BooleanField(default=True)
+    short_name = models.CharField(max_length=40)
+
+class User(models.Model):
+    user = models.ForeignKey(User)
+    identifier = models.CharField(max_length=32)
+
+def register(identifier):
+    #app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
+    model = models.get_model(app_label, model_name)
+    qs = model._default_manager.all()
+    print model
