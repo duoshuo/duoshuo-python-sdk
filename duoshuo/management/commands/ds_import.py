0b08939394bda8728812f2b7134a3353a30c8253
perchouli <jp.chenyang@gmail.com>
2012-07-22 10:18:48 +0800

    FIX: remove testing model

--- duoshuo/management/commands/ds_import.py
+++ duoshuo/management/commands/ds_import.py
@@ -3,6 +3,8 @@ from django.core.management.base import NoArgsCommand
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from django.contrib.comments.models import Comment
+from django.contrib.sites.models import Site
+
 import sys 
 reload(sys) 
 sys.setdefaultencoding('utf-8') 
@@ -43,5 +45,10 @@ class Command(BaseCommand):
             #response = urllib2.urlopen(api_url, data).read()
 
         if args[0] == 'comment':
-            comments = Comment.object.all()
-            
+            current_site = Site.objects.get_current()
+            if current_site.domain == 'example.com':
+                raise CommandError('I need to know your domain name, it should not be example.com')
+            comments = Comment.objects.all()
+
+        else:
+            raise CommandError('Tell me wha\'s data you want synchronization (user/article/comment)')
--- duoshuo/models.py
+++ duoshuo/models.py
@@ -2,7 +2,4 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class DuoshuoUser(models.Model):
-	user = models.ForeignKey(User)
-	duoshuo_id = models.IntegerField(default=0)
 	
