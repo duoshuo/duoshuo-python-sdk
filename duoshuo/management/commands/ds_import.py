9ce77644a7d57c45908cff5ac8318bed7ac1bd47
perchouli <jp.chenyang@gmail.com>
2012-08-01 20:16:29 +0800

    FIX: article replaced by thread

--- duoshuo/management/commands/ds_import.py
+++ duoshuo/management/commands/ds_import.py
@@ -23,7 +23,7 @@ DUOSHUO_SECRET = getattr(settings, "DUOSHUO_SECRET", None)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not args:
-            raise CommandError('Tell me what\'s data you want synchronization (user/article/comment)')
+            raise CommandError('Tell me what\'s data you want synchronization (user/thread/comment)')
         if not DUOSHUO_SHORT_NAME or not DUOSHUO_SECRET:
             raise CommandError('Before you can sync you need to set DUOSHUO_SHORT_NAME and DUOSHUO_SECRET')
         else:
@@ -67,8 +67,8 @@ class Command(BaseCommand):
                 data['posts[%s][created_at]'% comment.id] = comment.user_email,
                 data['posts[%s][message]'% comment.id] = comment.comment,
                 data['posts[%s][source]'% comment.id] = 'import',
-                try:
-                    thread_id = threads['%s_%s_%s' % (comment.content_type.app_label,comment.content_type.model,comment.content_object.id)]
+                thread_id:
+                    try = threads['%s_%s_%s' % (comment.content_type.app_label,comment.content_type.model,comment.content_object.id)]
                 except:
                     pass
                 else:
@@ -77,7 +77,7 @@ class Command(BaseCommand):
             print '%d %s was success sync;' % (len(comments), len(comments) > 1 and 'comments' or 'comment')
 
 
-        elif args[0] == 'article':
+        elif args[0] == 'thread':
             api_url = '%s://%s.duoshuo.com/api/threads/import.json' % (api.uri_schema, DUOSHUO_SHORT_NAME)
 
             current_site = Site.objects.get_current()
@@ -86,27 +86,27 @@ class Command(BaseCommand):
             else:
                 print "\033[0;32;40mAll threads will be import to %s, use Ctrl-D/Ctrl+C to break if this domain name is not correct.\033[0m" % current_site.domain
 
-            _s = raw_input('Please input the article model name such as `posts.article`:')
+            _s = raw_input('Please input the thread model name such as `threads.thread`:')
             if len(_s.split('.')) != 2:
                 raise CommandError('Model name is invalid.')
             else:
                 print "\033[0;32;40mStart  import thread from %s:\033[0m" % _s
                 app_label, model_name = [s.lower() for s in _s.split('.')]
 
-            article_model = get_model(app_label, model_name)
-            if not article_model:
+            thread_model = get_model(app_label, model_name)
+            if not thread_model:
                 raise CommandError('Cant\'t find model: %s.' % _s)
 
             try:
-                article_model.get_absolute_url
+                thread_model.get_absolute_url
             except AttributeError:
                 raise CommandError('Please define a get_absolute_url() method.')
 
             thread_schema = {'title': '', 'content': ''}
-            thread_schema['title'] = raw_input('Enter article title filed name: ')
-            thread_schema['content'] = raw_input('Enter article content filed name: ')
+            thread_schema['title'] = raw_input('Enter thread title filed name: ')
+            thread_schema['content'] = raw_input('Enter thread content filed name: ')
 
-            threads = article_model.objects.all()
+            threads = thread_model.objects.all()
             for thread in threads:
                 data['threads[%s][thread_key]'% thread.id] = '%s_%s_%s' % (app_label, model_name, str(thread.id))
                 data['threads[%s][url]'% thread.id] = 'http://%s%s' % (current_site.domain, thread.get_absolute_url())
@@ -130,7 +130,7 @@ class Command(BaseCommand):
             print '%d %s was success sync;' % (len(threads), len(threads) > 1 and 'threads' or 'thread')
 
         else:
-            raise CommandError('Tell me what\'s data you want synchronization (user/article/comment)')
+            raise CommandError('Tell me what\'s data you want synchronization (user/thread/comment)')
 
     # def _comment_to_threads(self, comment):
     #     thread = comment.
